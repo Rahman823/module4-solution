4f19f7eee640df0a50b5d181d0ce29193a1f8aee
rahman <loveyougit@.com>
2021-02-06 03:23:51 +0530

    completed module4-solution

---
+++ solution/app.js
@@ -0,0 +1,16 @@
+var names = ["yaakov", "rahman", "john", "Joe"];
+
+for(var i = 0; i < 4; i++) {
+	name = names[i];
+	if(name[0] == 'j' || name[0] == 'J') {
+
+		byeSpeaker.speak(name);
+	}
+	else {
+		helloSpeaker.speak(name);
+	}
+}
+// for(var i = 0; i < 4; i++) {
+// 	name = names[i]
+// 	console.log(name[0]);
+// }
