485c6bdd573d762597b38fce099ddaa38bdb2f92
rahman <loveyougit@.com>
2021-02-06 04:00:13 +0530

    completed-2 module4-solution

--- solution/script.js
+++ solution/script.js
@@ -1,6 +1,6 @@
-var names = ["yaakov", "rahman", "john", "Joe"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
 
-for(var i = 0; i < 4; i++) {
+for(var i = 0; i < 10; i++) {
 	name = names[i];
 	if(name[0] == 'j' || name[0] == 'J') {
 
